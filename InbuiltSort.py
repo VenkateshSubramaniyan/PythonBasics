dfc0f93bcbf832644b97c44b878beea0e0745d7a
VenkateshSubramaniyan <srvenkat16@gmail.com>
2024-05-26 14:13:10 +0530

    Heap and HeapSort

--- InbuiltSort.py
+++ InbuiltSort.py
@@ -13,3 +13,19 @@ intervals= [[1,4],[2,3]]
 print(intervals)
 intervals=sorted(intervals,key= lambda x:x[0])
 print(intervals)
+
+output=[]
+start,end=None,None
+for interval in intervals:
+    if start is None:
+        start=interval[0]
+        end=interval[1]
+    elif interval[0]<=end:
+        end=max(end, interval[1])
+    elif interval[0]>end:
+        output.append([start, max(end, interval[1])])
+        start=interval[0]
+        end=interval[1]
+
+output.append([start,end])
+print("ouput, ",output)
